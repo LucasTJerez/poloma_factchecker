84ce47f80621af34762fb384c2f069a85517cc0e
Lucas Jerez <ltj2105@columbia.edu>
2020-08-29 18:26:12 +0200

    Updates

---
+++ precache-manifest.0453f99487ee357cf1b18ae783a7660b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187654cb68a11cb9e1806eabb3d4a51f",
+    "url": "/poloma_factchecker/index.html"
+  },
+  {
+    "revision": "87b524e0c2181569a362",
+    "url": "/poloma_factchecker/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e812ff5faa8693981948",
+    "url": "/poloma_factchecker/static/js/2.f56976e8.chunk.js"
+  },
+  {
+    "revision": "87b524e0c2181569a362",
+    "url": "/poloma_factchecker/static/js/main.d9bb8341.chunk.js"
+  },
+  {
+    "revision": "5dfe4d4d08b495e7555a",
+    "url": "/poloma_factchecker/static/js/runtime~main.3b533505.js"
+  },
+  {
+    "revision": "e0b99454eedd9db3f13b8c0e091949e5",
+    "url": "/poloma_factchecker/static/media/booker.e0b99454.png"
+  },
+  {
+    "revision": "97239cf45467e019755920b5b91f689a",
+    "url": "/poloma_factchecker/static/media/bullock.97239cf4.png"
+  },
+  {
+    "revision": "77047a621351ea35e3b0ec054caf581a",
+    "url": "/poloma_factchecker/static/media/logo_blue_bg.77047a62.png"
+  },
+  {
+    "revision": "36b81f35aa29a44e1e48b0a26563cec1",
+    "url": "/poloma_factchecker/static/media/rubio.36b81f35.png"
+  },
+  {
+    "revision": "a81e5df24ab96731ff00efd9a34eac58",
+    "url": "/poloma_factchecker/static/media/ryan.a81e5df2.png"
+  },
+  {
+    "revision": "6bdaa60de777a4bdb06b2605bdd03fea",
+    "url": "/poloma_factchecker/static/media/trump.6bdaa60d.png"
+  }
+]);
